516b45efb452e1081e3dc38741d141d26882261c
Student_GA110 <Student_GA110@Student-GA110.local>
2020-03-12 12:38:23 -0400

    pseudocode

---
+++ pseudocode.js
@@ -0,0 +1,11 @@
+// First event rolls the dice to determine which player goes first.
+/*  if user1>user2 {
+    user1's Turn function initiates
+    else user2  Turn function initiates}
+
+    user1 or user2 Turn function call the roll dice function
+    and assings value to users position.
+    
+    The user position current number/value makes the users game piece display on that number on the board
+
+) */
