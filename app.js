dbd69a4e65633a34b03701b227c6879399ce418f
Student_GA110 <Student_GA110@Student-GA110.local>
2020-03-12 14:10:30 -0400

    functions

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
+let gameStart=false;
 
-let game = [{
-    name: "Player1",
-    position: ""
+let user = [{
+    name: "PLAYER1",
+    position: "",
+    turn: ""
 }, {
-    name: "Player2",
-    position: ""
+    name: "PLAYER2",
+    position: "",
+    turn: ""
 
 }]
 
-console.log(use)
+console.log(user[0].name)
+
+function firstTurn() {
+if (gameStart==false){
+    gameStart=true;
+
+}
+}
 
 
 
+function dice() {
+rolled= Math.floor(Math.random())
+}
+
+function user1(){
+
+}
+
+function user2(){
+
+}
+
+function trapIncrease(){
+
+}
+
+function trapDecrease(){
+
+}
