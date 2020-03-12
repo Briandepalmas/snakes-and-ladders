7b344542fc9b0d85315e53cf35ded44e50fd62e5
Student_GA110 <Student_GA110@Student-GA110.local>
2020-03-12 15:55:11 -0400

    first turn readys

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let gameStart = false;
+
 
 let user = [{
     name: "PLAYER1",
@@ -10,6 +10,14 @@ let user = [{
     turn: ""
 
 }]
+let gameStart = false;
+let position1= user[0].position
+let position2 = user[1].position
+
+
+firstTurn()
+//console.log(position1 + "xx" + rolled)
+
 
 function dice() {
     rolled = Math.floor(Math.random() * (7 - 1) + 1)
@@ -17,29 +25,78 @@ function dice() {
 
 function firstTurn() {
     if (gameStart == false) {
-        gameStart = true;
+
         user1()
         user2()
-        if (user[0].position>user[1].position){
+        console.log(position1)
+        console.log(position2)
+        if (position1 > position2) {
+            console.log("user1 goes first")
+            gameStart = true;
+            position1=0
+            position2=0
             user1();
+            
+
+        } else if(position1<position2){
+            console.log("user2 goes first")
+            gameStart = true;
+            position1=0
+            position2=0
+            user2()
+        }
+        else if(position1==position2){
+            console.log("tie")
+            firstTurn()
         }
     }
+    
 }
+
+
+
+
+
+
 
 function user1() {
     dice()
-    user[0].position=rolled
-    if (){
+    
+    if (gameStart == false) {
+        position1= rolled
 
+        console.log("22222")
+        console.log(gameStart)
+    }else{
+        console.log("1111")
+        position1+=rolled
+        console.log(rolled)
+        console.log(position1)
+        console.log(gameStart)
     }
 
-}
-user1()
 
-console.log(user[0].position+ " "+rolled)
+
+}
+
+
 function user2() {
+    dice()
+    
+    if (gameStart == false) {
+        position2= rolled
+        console.log("####")
+        console.log(gameStart)
+    }
+    if (gameStart==true){
+        console.log(rolled)
+        position2+=rolled
+        console.log(position2)
+    }
+
 
 }
+
 
 function trapIncrease() {
 
